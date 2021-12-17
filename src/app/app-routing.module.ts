2a9ca4eb8effa05371222aea9f6ab6a87f03d71a
Mjaiswal50 <jaiswalmayank450@gmail.com>
2021-12-18 01:40:45 +0530

    Auth Login Page Created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,11 @@ const routes: Routes = [
     path: '', component: HomeComponent
   },
   { path: 'counter', loadChildren: () => import('./counter/counter.module').then(m => m.CounterModule) },
-  { path: 'posts', loadChildren:()=>import('./posts/posts.module').then(m => m.PostsModule) }
+  { path: 'posts', loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule) },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  }
 ];
 
 @NgModule({
