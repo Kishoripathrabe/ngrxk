d6b448cbc01e9294f1647dc03fcb2dfae213602b
Mjaiswal50 <jaiswalmayank450@gmail.com>
2021-12-30 02:27:42 +0530

    add guard in posts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CounterComponent } from './counter/counter.component';
 import { HomeComponent } from './home/home.component';
+import { AuthGuard } from './services/auth.guard';
 
 
 const routes: Routes = [
@@ -9,7 +10,7 @@ const routes: Routes = [
     path: '', component: HomeComponent
   },
   { path: 'counter', loadChildren: () => import('./counter/counter.module').then(m => m.CounterModule) },
-  { path: 'posts', loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule) },
+  { path: 'posts', loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule),canActivate:[AuthGuard] },
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
